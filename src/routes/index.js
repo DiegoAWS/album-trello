dbe9b464b619f35d4a18743dbc589b0841eaf3ff
DiegoCuba <diego.netsolutions@gmail.com>
2021-04-10 00:27:51 -0400

    [fix] completly ride of REQUEST instead of AXIOS

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request');
 const querystring = require('querystring');
 const _get = require('lodash/get');
 
@@ -11,82 +10,68 @@ router.post('/getPicture', async (req, res) => {
   const client_id = process.env.SPOTIFY_ID;
   const client_secret = process.env.SPOTIFY_SECRET;
 
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
+
+
+  axios({
+    url: "https://accounts.spotify.com/api/token",
+    method: "post",
+    params: {
+      grant_type: "client_credentials"
+    },
     headers: {
-      'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64')
+      "Accept": "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
     },
-    form: {
-      grant_type: 'client_credentials'
-    },
-    json: true
-  };
-
-  request.post(authOptions, function (error, response, body) {
-
-    if (!error && response?.statusCode === 200 && body?.access_token) {
-
-      const { access_token } = body;
-
-
-      const list = req.body;
-      const totalElements = list.length;
-
-
-
-      const getOneAlbum = (album, year) => {
-        const query = {
-          q: album + ' ' + 'year:' + year,
-          type: 'album',
-        }
-
-        
-        const url = 'https://api.spotify.com/v1/search?' + querystring.stringify(query);
-        const options = {
-          headers: {
-            'Authorization': 'Bearer ' + access_token
-          },
-          json: true
-        };
-        return axios.get(url, options);
-
-      }
-      var p = [];
-      for (let i = 0; i < totalElements; i++) {
-        const album = list[i].album
-        const year = list[i].year
-
-        p.push(getOneAlbum(album, year))
-      }
-
-      Promise.all(p).then((data) => {
-
-        const albumsOK = data.map(item => ({
-          album_picture: _get(item, 'data.albums.items[0].images[0].url', ''),
-          album_name: _get(item, 'data.albums.items[0].name', ''),
-          album_artist: _get(item, 'data.albums.items[0].artists[0].name', ''),
-          album_year: _get(item, 'data.albums.items[0].release_date', '').slice(0, 4),
-        }))
-        res.json(albumsOK)
-
-      }).catch(err => {
-        console.log('error ', err)
-        res.json(err)
-      })
-
-      // request.get(options, function (error, response, body) {
-      //   const album_pict = _get(body, 'albums.items[0].images[0].url', '');
-
-      //   resp.push({ album, year, album_pict })
-      //   if (resp.length === totalElements)
-      //     res.json(resp)
-      // });
-
-
-
+    auth: {
+      username: client_id,
+      password: client_secret
     }
-    else
-      res.send('error')
+  }).then(data => {
+
+    const { access_token } = data?.data;
+
+
+
+
+    const getOneAlbum = (album, year, token) => {
+
+      const query = { q: album + ' ' + 'year:' + year, type: 'album', }
+
+      const options = {
+        url: 'https://api.spotify.com/v1/search?' + querystring.stringify(query),
+        headers: { 'Authorization': 'Bearer ' + token }
+      };
+      return axios(options);
+    }
+
+    var p = [];
+    const list = req.body;
+    for (let i = 0; i < list.length; i++) {
+      const album = list[i].album
+      const year = list[i].year
+
+      p.push(getOneAlbum(album, year, access_token))
+    }
+
+    Promise.all(p).then((data) => {
+
+      const albumsOK = data.map(item => ({
+        album_picture: _get(item, 'data.albums.items[0].images[0].url', ''),
+        album_name: _get(item, 'data.albums.items[0].name', ''),
+        album_artist: _get(item, 'data.albums.items[0].artists[0].name', ''),
+        album_year: _get(item, 'data.albums.items[0].release_date', '').slice(0, 4),
+      }))
+      res.json(albumsOK)
+
+    }).catch(err => {
+      console.log('error REQUESTING ALBUMS', err)
+      res.json(err)
+    })
+
+
+  }).catch(err => {
+    console.log('error TOKEN', { err })
+    res.send('error')
   });
 
 })
